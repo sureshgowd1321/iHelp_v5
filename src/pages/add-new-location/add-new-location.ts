723f1309f0b2f063fd5eb101243dd4ac5ba8a7a8
Suresh Goud <sureshgowd1321@gmail.com>
2018-05-18 21:54:26 +0530

    Add new location Done

--- src/pages/add-new-location/add-new-location.ts
+++ src/pages/add-new-location/add-new-location.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+
+// Providers
+import { PhpServiceProvider } from '../../providers/php-service/php-service';
 
 /**
  * Generated class for the AddNewLocationPage page.
@@ -15,11 +19,30 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class AddNewLocationPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  public newLocationForm: FormGroup;
+
+  constructor(public navCtrl: NavController, 
+              public navParams: NavParams,
+              public formBuilder: FormBuilder,
+              public phpService: PhpServiceProvider) {
+
+      this.newLocationForm = formBuilder.group({
+        country: ['', Validators.compose([Validators.minLength(1), Validators.required])],
+        state  : ['', Validators.compose([Validators.minLength(1), Validators.required])],
+        city   : ['', Validators.compose([Validators.minLength(1), Validators.required])]
+      });
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad AddNewLocationPage');
-  }
+  // Add New location and Go back to previous page
+  addNewLocation() {
 
+    if (!this.newLocationForm.valid) {
+      console.log(this.newLocationForm.value);
+    } else {
+      this.phpService.addNewLocation(this.newLocationForm.value.country, this.newLocationForm.value.state, this.newLocationForm.value.city).subscribe(newLocationInfo => {
+        this.navCtrl.pop();
+      }); 
+    }
+
+  }
 }
